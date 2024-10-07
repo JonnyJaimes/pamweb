f00e71592caa73af2ea5a4267605fb6d6504ea25
Jonathan Mauricio Vargas Bolivar <71857159+MIGMAUXD@users.noreply.github.com>
2024-10-07 10:33:42 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://3.81.249.9:8080',
+  apiBaseUrl: 'https://3.81.249.9:8080',
   // apiBaseUrl: 'https://pam-api.herokuapp.com',
 
 };
